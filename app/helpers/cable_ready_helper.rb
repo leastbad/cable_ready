dd3978a82a18359519cda30ef182485a3a0fc8ab
ParamagicDev <konnor5456@gmail.com>
2021-02-03 14:43:57 -0500

    fix my mistake!

--- app/helpers/cable_ready_helper.rb
+++ app/helpers/cable_ready_helper.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
-module CableReady
-  module CableReadyHelper
-    include Compoundable
+module CableReadyHelper
+  include CableReady::Compoundable
 
-    def stream_from(*keys)
-      keys.select!(&:itself)
-      identifier = keys.one? ? keys.pop : compound(keys)
-      if identifier.class < ActiveRecord::Base
-        return tag.div(data: {controller: "stream-from", stream_from_sgid_value: identifier.to_sgid(expires_in: nil).to_s})
-      end
-      tag.div(data: {controller: "stream-from", stream_from_identifier_value: identifier.to_s})
+  def stream_from(*keys)
+    keys.select!(&:itself)
+    identifier = keys.one? ? keys.pop : compound(keys)
+    if identifier.class < ActiveRecord::Base
+      return tag.div(data: {controller: "stream-from", stream_from_sgid_value: identifier.to_sgid(expires_in: nil).to_s})
     end
+    tag.div(data: {controller: "stream-from", stream_from_identifier_value: identifier.to_s})
   end
 end
