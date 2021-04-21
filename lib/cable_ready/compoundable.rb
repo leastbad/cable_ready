c4b1d2a29e354b703a2fa4e709adcbb417586f2e
Julian Rubisch <julian@julianrubisch.at>
2021-04-21 09:12:44 +0200

    Signing once is enough

--- lib/cable_ready/compoundable.rb
+++ lib/cable_ready/compoundable.rb
@@ -4,7 +4,7 @@ module CableReady
   module Compoundable
     def compound(keys)
       keys.map { |key|
-        key.class < ActiveRecord::Base ? key.to_sgid(expires_in: nil).to_s : key.to_s
+        key.class < ActiveRecord::Base ? key.to_global_id.to_s : key.to_s
       }.join(":")
     end
   end
